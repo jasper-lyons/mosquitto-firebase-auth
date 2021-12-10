86029c28e8aca917af15ae30316032bd234d727c
Jasper Lyons <jasper.lyons@gmail.com>
2021-12-10 22:24:13 +0000

    feat: add initial implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mosquitto Firebase Auth
+
+This is an auth plugin for [Mosquitto](https://github.com/eclipse/mosquitto) which uses [mosquito_pyauth](https://github.com/jasper-lyons/mosquitto_pyauth) to load a python module which uses the [firebase_admin](https://github.com/firebase/firebase-admin-python) library to authenticate users submitting messages to mosquitto instance via Google Firebase.
+
+
---
+++ mosquitto/mosquitto_firebase_auth.py
@@ -0,0 +1,30 @@
+import os
+from mosquitto_auth import log, LOG_INFO
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+
+def plugin_init(opts):
+    log(LOG_INFO, 'firebase auth: starting up!')
+    firebase_admin.initialize_app(credentials.Certificate(os.environ.get(
+        'FIREBASE_SERVICE_ACCOUNT_PATH',
+        '.firebase-service-account.json'
+    ))
+
+def unpwd_check(username, password):
+    try:
+        auth.verify_id_token(password)
+        return True
+    except auth.RevokedIdTokenError:
+        log(LOG_INFO, f"{username}'s token was revoked!")
+    except auth.UserDisabledError:
+        log(LOG_INFO, f"{username} has been disabled!")
+    except auth.InvalidTokenError:
+        log(LOG_INFO, f"{username} provided an invalid token!")
+    return False
+
+def acl_check(client_id, username, topic, access, payload):
+    return True
+
+def psk_key_get(identity, hint):
+    return ''
